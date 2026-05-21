79d3e7110846b6df4e21d4ca7271558470b87fe3
GreenHopeRise <khalidanwarantur96@gmail.com>
2026-05-21 22:14:52 +0600

    stucture building

--- src/auth/RoleRoute.jsx
+++ src/auth/RoleRoute.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { useAuth } from './AuthContext'
-import { Navigate } from 'react-router-dom'
-import { can } from '@/lib/can'
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
-const RoleRoute = ({children, allow}) => {
-    const {user,loading} = useAuth()
-    if(loading) return <p>Checking...</p>
-    if(!user) return <Navigate to='/login' replace/>
-    if(!can(user.role, perm))
-  return children
-}
+const RoleRoute = ({ children, allow }) => {
+  const { user, loading } = useAuth();
 
-export default RoleRoute
+  if (loading) return <p>Checking...</p>;
+
+  if (!user) return <Navigate to="/login" replace />;
+
+  if (!allow.includes(user.role)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default RoleRoute;
